3cc9a377e4a207bf8e01077b4dfd520801ff406d
Archit-Jain05 <architjain2005@gmail.com>
2023-07-18 17:28:46 +0530

    1

---
+++ addition.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php 
+        $var1 = 10;
+        $var2 = 14;
+
+        $var3 = $var1+$var2;
+        echo $var3;
+        ?>
+    </body>
+</html>
---
+++ variable.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <?php 
+        $var1 = 'hii';
+        echo $var1;
+        ?>
+    </body>
+</html>
